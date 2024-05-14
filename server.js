1dd1165d2f0ae8bfda3cc46eb767e99e054f35ca
megadogalo <mylene.jpeg@gmail.com>
2024-05-13 23:51:57 -0300

    upando

--- server.js
+++ server.js
@@ -1,10 +1,16 @@
 const express = require('express')
+const.mongoose
 const app = express()
 
 //routes
 
 app.get('/',  (req, res) => {
     res.send('Oii aqui eh a mi')
+})
+
+app.get('/GALO', (req, res) => {
+    res.send('AQUI EH GALO')
+
 })
 
 app.listen(3000, () => {
